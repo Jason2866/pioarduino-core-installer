9bd8e496e8ccff8779f387719fe46fb4fb9ea666
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-08-30 12:53:52 +0200

    Update email and contact information

--- pioinstaller/__init__.py
+++ pioinstaller/__init__.py
@@ -22,7 +22,8 @@ __description__ = "An installer for PlatformIO pioarduino Core"
 __url__ = "https://github.com/pioarduino/pioarduino-core-installer"
 
 __author__ = "PlatformIO Labs and pioarduino"
-__email__ = "https://discord.gg/Nutz9crnZr"
+__contact__ = "https://discord.gg/Nutz9crnZr"
+__email__ = ""
 
 
 __license__ = "Apache-2.0"
